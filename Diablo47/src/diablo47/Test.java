a0ab7afcf562cc0c2f448b44433f1cdfb69cf01f
USER <USER@192.168.0.7>
2016-08-30 16:34:06 +0900

    test2

--- Diablo47/src/diablo47/Test.java
+++ Diablo47/src/diablo47/Test.java
@@ -1,7 +1,9 @@
 package diablo47;
 
 public class Test {
-public static void main(String[] args) {
-	System.out.println("testtset");
-}
+	public static void main(String[] args) {
+		System.out.println("testtset");
+		
+		System.out.println("abc");
+	}
 }
